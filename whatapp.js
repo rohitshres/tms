55788779b8c732bd8ce70b2024596fc763252f02
Rohit Shrestha <rohit.shrestha1@codingmountain.com>
2025-06-26 20:53:20 +0545

    test

--- whatapp.js
+++ whatapp.js
@@ -4,7 +4,19 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { handleSIGINT: true }
+    puppeteer: {
+        handleSIGINT: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
+    }
 });
 
 client.on('qr', (qr) => {
